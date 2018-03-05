9e78e2a30ef4d1c0c561f4e3786a0b61240d9dd5
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-03-05 14:26:44 -0800

    Create and test #rooms_and_reservations in ReservationManager

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -5,5 +5,13 @@ module Hotel
     def initialize
     end # initialize
 
+    def rooms_and_reservations
+      rooms_and_reservations = {}
+      20.times do |room|
+        rooms_and_reservations[room + 1] = []
+      end
+      rooms_and_reservations
+    end
+
   end # class ReservationManager
 end # module Hotel
--- specs/reservation_manager_spec.rb
+++ specs/reservation_manager_spec.rb
@@ -11,4 +11,28 @@ describe Hotel::ReservationManager do
     end
   end # initialize
 
+  describe "#rooms_and_reservations" do
+    before do
+      @rooms_and_reservations = @reservation_manager.rooms_and_reservations
+    end
+
+    it "returns a hash" do
+      @rooms_and_reservations.must_be_instance_of Hash
+    end
+
+    it "returns a hash of length 20, for which every key is a number from 1 to 20" do
+      array_to_twenty = (1..20).map do |number|
+        number
+      end
+      @rooms_and_reservations.size.must_equal 20
+      @rooms_and_reservations.keys.must_equal array_to_twenty
+    end
+
+    it "returns a hash for which every value is an empty array upon creation of ReservationManager" do
+      @rooms_and_reservations.each do |room, reservation|
+        reservation.must_equal []
+      end
+    end
+  end # rooms_and_reservations
+
 end # describe ReservationManager
