021d7f262040835417644c022b39b604673d6caa
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-03-05 12:39:52 -0800

    Add and test Reservation#initialize

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,8 +1,13 @@
 module Hotel
   class Reservation
-    
-    def initialize
-    end
 
+    attr_reader :room_number
+
+    def initialize(start_date, end_date)
+      @room_number = rand(21)
+      @start_date = Date.parse(start_date)
+      @end_date = Date.parse(end_date)
+    end
+    
   end # class Reservation
 end # module Hotel
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -2,11 +2,19 @@ require_relative 'spec_helper'
 
 describe Hotel::Reservation do
 
+  before do
+    @reservation_one = Hotel::Reservation.new('2018-04-05', '2018-04-10')
+  end
+
   describe "#initialize" do
     it "can be created" do
+      @reservation_one.must_be_instance_of Hotel::Reservation
     end
 
     it "selects a valid room number" do
+      @reservation_one.must_respond_to :room_number
+
+      (1..20).must_include @reservation_one.room_number
     end
 
     it "returns an error for an invalid date range" do
