afd930531812eb09c672e33bc98aff7fda4358e9
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-03-06 16:09:01 -0800

    Add and test #initialize to Block class

--- lib/block.rb
+++ lib/block.rb
@@ -1,7 +1,21 @@
+require_relative 'reservation_manager'
+
 module Hotel
 
   class Block
-    
+
+    def initialize(num_rooms, start_date, end_date, discount_rate)
+      @num_rooms = num_rooms
+      @start_date = start_date
+      @end_date = end_date
+      @discount_rate = discount_rate
+      @block = generate_block
+
+      if num_rooms > 5
+        raise ArgumentError.new("Error: cannot set aside more than 5 rooms in a block.")
+      end
+    end
+
   end # class Block
 
 end # module Hotel
--- specs/block_spec.rb
+++ specs/block_spec.rb
@@ -1,4 +1,17 @@
 require_relative 'spec_helper'
 
-describe Helper::Block do
-end 
+describe Hotel::Block do
+
+  describe "#initialize" do
+    it "can be created" do
+      block = Hotel::Block.new(5, "2018-05-05", "2018-05-10", 150.00)
+      block.must_be_instance_of Hotel::Block
+    end
+
+    it "raises an error if you try to put more than 5 rooms in a block" do
+      block = proc { Hotel::Block.new(6, "2018-05-05", "2018-05-10", 150.00) }
+      block.must_raise ArgumentError
+    end
+  end # initialize
+
+end # describe Helper::Block
